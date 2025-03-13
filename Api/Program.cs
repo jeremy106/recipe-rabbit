712323f532e2c37274702f78895f0c73e33baf8d
Jeremy Webster <jeremy.webster106@gmail.com>
2025-03-14 11:34:27 +1300

    setup react app

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,11 +4,19 @@ using RecipeRabbit.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
 builder.Services.AddDbContext<RecipeContext>(opt =>
   opt.UseInMemoryDatabase("RecipeRabbitDb"));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder => builder.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
 
 var app = builder.Build();
 
@@ -23,6 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowReactApp");
 
 app.UseAuthorization();
 
