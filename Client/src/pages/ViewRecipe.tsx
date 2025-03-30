a085b27b9c85689233a1aa34e6587490e816ec5d
Jeremy Webster <jeremy.webster106@gmail.com>
2025-03-31 11:48:24 +1300

    add delete button

--- Client/src/pages/ViewRecipe.tsx
+++ Client/src/pages/ViewRecipe.tsx
@@ -1,15 +1,18 @@
-import { useQuery } from "@tanstack/react-query"
-import { getRecipeById } from "../api/recipes"
-import { useNavigate, useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query'
+import { getRecipeById } from '../api/recipes'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import styles from '../styles/ViewRecipe.module.scss'
 
 export default function ViewRecipe() {
-
   const { id: recipeId } = useParams()
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
-  const { data: recipe, isPending, isError } = useQuery({
+  const {
+    data: recipe,
+    isPending,
+    isError,
+  } = useQuery({
     queryKey: ['recipe', recipeId],
     queryFn: () => getRecipeById(String(recipeId)),
   })
@@ -22,44 +25,62 @@ export default function ViewRecipe() {
     return <p>Error</p>
   }
 
-  function handleClick(){
+  function handleEdit() {
     navigate('edit')
   }
 
+  function handleDelete() {
+    alert('Coming soon')
+  }
 
   return (
     <>
-    <h1>{recipe.name}</h1>
-    <div className="m-container">
-      <div className={styles['recipe-info']}>
-        <p><span className="emphasis">Author:</span> {recipe.author ?? "unknown"}</p>
-        <p><span className="emphasis">Source:</span> {recipe.source ?? "unknown"}</p>
-        <p><span className="emphasis">Serves:</span> {recipe.servings ?? "unknown"}</p>
-        <p><span className="emphasis">Prep Time:</span> {recipe.prepTime ?? "unknown"}</p>
-        <p><span className="emphasis">Cooking Time:</span> {recipe.cookTime ?? "unknown"}</p>
-      </div>
-      <div className={styles["ingredient-list"]}>
-        <h3>Ingredients:</h3>
-        {
-          recipe.ingredients.map((ingredient) => (
+      <h1>{recipe.name}</h1>
+      <div className="m-container">
+        <div className={styles['recipe-info']}>
+          <p>
+            <span className="emphasis">Author:</span>{' '}
+            {recipe.author ?? 'unknown'}
+          </p>
+          <p>
+            <span className="emphasis">Source:</span>{' '}
+            {recipe.source ?? 'unknown'}
+          </p>
+          <p>
+            <span className="emphasis">Serves:</span>{' '}
+            {recipe.servings ?? 'unknown'}
+          </p>
+          <p>
+            <span className="emphasis">Prep Time:</span>{' '}
+            {recipe.prepTime ?? 'unknown'}
+          </p>
+          <p>
+            <span className="emphasis">Cooking Time:</span>{' '}
+            {recipe.cookTime ?? 'unknown'}
+          </p>
+        </div>
+        <div className={styles['ingredient-list']}>
+          <h3>Ingredients:</h3>
+          {recipe.ingredients.map((ingredient) => (
             <ul key={ingredient.name}>
-              <li>{ingredient.amount} {ingredient.name} {ingredient.notes && `(${ingredient.notes})`}</li>
+              <li>
+                {ingredient.amount} {ingredient.name}{' '}
+                {ingredient.notes && `(${ingredient.notes})`}
+              </li>
             </ul>
-          ))
-        }
-      </div>
-      <div className={styles['steps-container']}>
-        {
-          recipe.steps.map((step) => (
+          ))}
+        </div>
+        <div className={styles['steps-container']}>
+          {recipe.steps.map((step) => (
             <p key={step.stepOrder}>
-              <span className="emphasis">Step {step.stepOrder+1}: </span>
+              <span className="emphasis">Step {step.stepOrder + 1}: </span>
               {step.description}
             </p>
-          ))
-        }
+          ))}
+        </div>
+        <button onClick={handleEdit}>Edit this recipe</button>
+        <button onClick={handleDelete}>Delete this recipe</button>
       </div>
-      <button onClick={handleClick}>Edit this recipe</button>
-    </div>
     </>
   )
 }
